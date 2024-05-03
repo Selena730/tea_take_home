46b4915a002a9ab07ddfa325da454026da10e7c7
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-05-03 12:14:58 -0600

    Feat: create subscription  model

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,12 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: true
+  validates :status, presence: true
+  validates :frequency, presence: true
+  validates :tea_id, uniqueness: {scope: :customer_id, message: "subscription for this tea already exists" }
+
+  enum :status, [ :active, :cancelled ]
 end
