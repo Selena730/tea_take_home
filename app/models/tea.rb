368d9f8a47a97b237dba823e95b77786b68ca28d
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-05-03 12:15:16 -0600

    Feat: create tea model

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,8 @@
 class Tea < ApplicationRecord
+    has_many :subscriptions
+
+    validates :title, presence: true
+    validates :description, presence: true
+    validates :temperature, presence: true, numericality: true
+    validates :brew_time, presence: true, numericality: true
 end
