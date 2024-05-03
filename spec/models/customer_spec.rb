ee3c9dd93dfb5a22e4356f180d32e2391e66d532
Selena Hawamdeh <selenahawamdeh@gmail.com>
2024-05-03 12:18:03 -0600

    Test: all models

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Customer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do 
+    it { should have_many :subscriptions }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:address) }
+    it { should validate_uniqueness_of(:email) }
+  end
+
+  it "has a valid factory" do
+    customer = build(:customer)
+    expect(customer).to be_valid
+  end
+
+  it "creates a customer with all attributes" do
+    customer = create(:customer)
+    expect(customer.first_name).not_to be_nil
+    expect(customer.last_name).not_to be_nil
+    expect(customer.email).not_to be_nil
+    expect(customer.address).not_to be_nil
+  end
 end
--- spec/models/tea_spec.rb
+++ spec/models/tea_spec.rb
@@ -1,5 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe Tea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "relationships" do
+    it { should have_many :subscriptions }
+  end
+
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:description) }
+    it { should validate_presence_of(:brew_time) }
+    it { should validate_presence_of(:temperature) }
+    it { should validate_numericality_of(:brew_time) } 
+    it { should validate_numericality_of(:temperature) } 
+
+  end
+
+  it "has a valid factory" do
+    tea = build(:tea)
+    expect(tea).to be_valid
+  end
+
+  it "creates tea with realistic attributes" do
+    tea = create(:tea)
+    expect(tea.title).to be_a(String)
+    expect(tea.description).to be_a(String)
+    expect(tea.temperature).to be_between(60, 100)
+    expect(tea.brew_time).to be_between(1, 5)
+  end
 end
